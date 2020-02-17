efc869b396410f94b3cc90000a989f5efd5d78fa
jernicozz <cendleader@gmail.com>
2020-02-17 15:30:13 +0300

    Initial Commit

---
+++ mini-sql/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mini-sql
+//
+//  Created by Дмитрий Маслюков on 17.02.2020.
+//  Copyright © 2020 Дмитрий Маслюков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
